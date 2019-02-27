77be41b896606657c491c17c8465763360a4e0fc
fablecode <fablecode@users.noreply.github.com>
2019-02-27 01:01:12 +0000

    Solution folders added.

---
+++ src/ProductAdvertisingAPI/productadvertising/AmazonProductAdvertising.cs
@@ -0,0 +1,33 @@
+﻿namespace productadvertising
+{
+    public class AmazonProductAdvertising
+    {
+        public AmazonProductAdvertising(IAmazonSettings settings)
+        {
+            
+        }
+    }
+
+    public interface IAmazonSettings
+    {
+        AmazonAuthentication Authentication { get; set; }
+        AmazonAffiliate Affiliate { get; set; }
+    }
+
+    public class AmazonSettings : IAmazonSettings
+    {
+        public AmazonAuthentication Authentication { get; set; }
+        public AmazonAffiliate Affiliate { get; set; }
+    }
+
+    public class AmazonAuthentication
+    {
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+    }
+    public class AmazonAffiliate
+    {
+        public string EndPoint { get; set; }
+        public string AssociateTag { get; set; }
+    }
+}
